b1b1ce8850575edf92ba5852e9c66a1c981c1b98
secchanu <secchanu929@gmail.com>
2023-01-04 20:26:50 +0900

    update

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -6,30 +6,31 @@ import styles from "@/styles/[slug].module.css";
 import { readFileSync } from "fs";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
+import rehypeExternalLinks from 'rehype-external-links'
 
 type Props = {
-  item: {
-    content: string;
-  };
+	item: {
+		content: string;
+	};
 };
 const Page: NextPage<Props> = (props) => {
-  const item = props.item;
+	const item = props.item;
 
-  return (
-    <div className={styles.wrapper}>
-      <article className={`${styles["markdown-body"]} markdown-body`}>
-        <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-          {item.content}
-        </ReactMarkdown>
-      </article>
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<article className={`${styles["markdown-body"]} markdown-body`}>
+				<ReactMarkdown rehypePlugins={[rehypeRaw, () => rehypeExternalLinks({target: "_blank"})]	}>
+					{item.content}
+				</ReactMarkdown>
+			</article>
+		</div>
+	);
 };
 
 export default Page;
 
 export const getStaticProps: GetStaticProps = () => {
-  const content = readFileSync("resource/portfolio.md", "utf-8");
-  const item = { content };
-  return { props: { item } };
+	const content = readFileSync("resource/portfolio.md", "utf-8");
+	const item = { content };
+	return { props: { item } };
 };
